56009915932e2943895f8664396e86253a621b65
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-09-09 21:32:19 +0530

    .............

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import Video from '../Components/Home/Video'
+import HomeHeroText from '../Components/Home/HomeHeroText'
+import HomeBottomText from '../Components/Home/HomeBottomText'
 const Home = () => {
   return (
-    <div>
-      
+    <div className="">
+    <div className='h-screen w-screen '>
+
+      <Video />
+      home
+    </div>
+    <div className="h-screen w-screen relative flex flex-col justify-between items-center">
+      <HomeHeroText />
+      <HomeBottomText /> 
+    </div>
+    
+    
+    
     </div>
   )
 }
